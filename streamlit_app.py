a3a31e8a2cd9eebdc2c5f2ddbb9fa966fe387b98
Ben Poole <86692194+stratkit@users.noreply.github.com>
2024-11-27 17:41:19 +0000

    added dashboards and picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("You've Found Strategy Kit")
+st.write("Blue Dot Energy Membership Map")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Take a look at some data viz"
+)
+# Dashboard options
+dashboard_options = {
+    "Blue Dotters by ZIP": "https://public.tableau.com/views/BlueDotMembersbyZIPCode/BlueDotNEMembers?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link?:embed=y&:showVizHome=no",
+    "Nebraska Voters by Party": "https://public.tableau.com/views/NebraskaVotersbyParty2024/Sheet1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link?:embed=y&showVizHome=no",
+    "Campaign Analytics (draft)": "https://public.tableau.com/views/CampaignAnalytics-broadcast/Sheet2?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link?:embed=y&showVizHome=no",
+}
+
+# Dropdown to select a Tableau dashboard
+selected_dashboard = st.selectbox(
+    "Select a Dashboard to View:", list(dashboard_options.keys())
+)
+
+# Embed the selected dashboard
+st.components.v1.iframe(dashboard_options[selected_dashboard], width=800, height=650)
+
+
+# Footer
+st.write(
+    "Powered by [Streamlit](https://streamlit.io/), Tableau Public and StrategyKit."
 )
